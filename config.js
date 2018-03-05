eff4ffe631f745e5d07fd905cc32708b636bf157
Chung <chung@lakoo.com>
2018-03-05 13:06:11 +0800

    First commit

---
+++ config.js
@@ -0,0 +1,20 @@
+const config = {};
+
+config.FIRESTORE_TX_ROOT = '';
+
+config.GCLOUD_PUBSUB_MAX_MESSAGES = 10;
+config.GCLOUD_PUBSUB_MAX_WAIT = 1000;
+config.GCLOUD_PUBSUB_ENABLE = true;
+
+// Number of milliseconds before a transaction can enter the queue again
+config.TX_LOOP_INTERVAL = 30 * 1000;
+
+// Minimum number of milliseconds between API calls
+config.FETCH_INTERVAL = 1000;
+
+// Number of milliseconds before a transaction is set to timeout state
+config.TIME_LIMIT = 60 * 60 * 1000 * 2;
+
+module.exports = config;
+
+// vim: set ts=2 sw=2:
