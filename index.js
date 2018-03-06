3703802188298ab44384edbddd4d27d9bed72e56
Chung <chung@lakoo.com>
2018-03-06 15:07:22 +0800

    Refactor WatchQueue to class

--- index.js
+++ index.js
@@ -28,67 +28,72 @@ function sleep(ms) {
   return new global.Promise(resolve => setTimeout(resolve, ms));
 }
 
-const txQueue = [];
-
 const TIME_LIMIT = config.TIME_LIMIT || 60 * 60 * 1000 * 24; // fallback: 1 day
 const TX_LOOP_INTERVAL = config.TX_LOOP_INTERVAL || 30 * 1000; // fallback: 30s
 const FETCH_INTERVAL = config.FETCH_INTERVAL || 1000; // fallback: 1s
 const MAX_TX_IN_QUEUE = config.MAX_TX_IN_QUEUE || 1000;
 
-async function startWatcher() {
-  for (;;) {
-    const loopTime = Date.now();
-    const tx = txQueue.shift();
-    if (tx) {
-      const {
-        txHash,
-        data,
-        timestamp,
-        cb,
-      } = tx;
-      try {
-        const receipt = await web3.eth.getTransactionReceipt(txHash);
-        if (!receipt) {
-          const pendingTx = await web3.eth.getTransaction(txHash);
-          if (pendingTx) {
-            // The transaction is pending on Ethereum network but not mined yet, so should not
-            // be considered timeout too early
-            const payload = { txHash, nonce: data.nonce };
-            // eslint-disable-next-line no-console
-            console.log(`${new Date().toISOString()} got transaction but no receipt, payload: ${JSON.stringify(payload)}`);
-            tx.timestamp = Date.now();
-          }
-          if (Date.now() - timestamp > TIME_LIMIT) {
-            cb(STATUS_TIMEOUT, tx);
-          } else {
-            // wait for retry
-            setTimeout(() => txQueue.push(tx), TX_LOOP_INTERVAL);
-          }
-        } else if (Number.parseInt(receipt.status, 16) === 1) {
-          cb(STATUS_SUCCESS, tx, receipt);
-        } else {
-          cb(STATUS_FAIL, tx, receipt);
-        }
-      } catch (err) {
-        console.error(err); // eslint-disable-line no-console
-      }
-    }
-    const timeUsed = Date.now() - loopTime;
-    if (timeUsed < FETCH_INTERVAL) {
-      await sleep(FETCH_INTERVAL - timeUsed);
-    }
+class WatchQueue {
+  constructor() {
+    this.queue = [];
   }
-}
 
-function watchTx(doc, cb) {
-  const txHash = doc.id;
-  const data = doc.data();
-  txQueue.push({
-    timestamp: data.ts || Date.now(),
-    txHash,
-    data,
-    cb,
-  });
+  async startWatch() {
+    setImmediate(async () => {
+      for (;;) {
+        const loopTime = Date.now();
+        const tx = this.queue.shift();
+        if (tx) {
+          const { txHash, data, timestamp } = tx;
+          try {
+            const receipt = await web3.eth.getTransactionReceipt(txHash);
+            if (!receipt) {
+              const pendingTx = await web3.eth.getTransaction(txHash);
+              if (pendingTx) {
+                // The transaction is pending on Ethereum network but not mined yet, so should not
+                // be considered timeout too early
+                const payload = { txHash, nonce: data.nonce };
+                // eslint-disable-next-line no-console
+                console.log(`${new Date().toISOString()} got transaction but no receipt, payload: ${JSON.stringify(payload)}`);
+                tx.timestamp = Date.now();
+              }
+              if (Date.now() - timestamp > TIME_LIMIT) {
+                if (this.statusCb) {
+                  this.statusCb(STATUS_TIMEOUT, tx);
+                }
+              } else {
+                // wait for retry
+                setTimeout(() => this.queue.push(tx), TX_LOOP_INTERVAL);
+              }
+            } else if (this.statusCb) {
+              if (Number.parseInt(receipt.status, 16) === 1) {
+                this.statusCb(STATUS_SUCCESS, tx, receipt);
+              } else {
+                this.statusCb(STATUS_FAIL, tx, receipt);
+              }
+            }
+          } catch (err) {
+            console.error(err); // eslint-disable-line no-console
+          }
+        }
+        const timeUsed = Date.now() - loopTime;
+        if (timeUsed < FETCH_INTERVAL) {
+          await sleep(FETCH_INTERVAL - timeUsed);
+        }
+      }
+    });
+  }
+
+  add(doc) {
+    const txHash = doc.id;
+    const data = doc.data();
+    this.queue.push({ timestamp: data.ts || Date.now(), txHash, data });
+  }
+
+  setStatusCallback(cb) {
+    this.statusCb = cb;
+    return this;
+  }
 }
 
 function statusCallback(status, tx, receipt) {
@@ -120,15 +125,17 @@ function statusCallback(status, tx, receipt) {
 }
 
 function main() {
+  const watchQueue = new WatchQueue();
+  watchQueue.setStatusCallback(statusCallback);
   const txRef = db.collection(config.FIRESTORE_TX_ROOT);
   txRef.where('status', '==', 'pending')
     .orderBy('ts')
     .limit(MAX_TX_IN_QUEUE)
     .onSnapshot((snapshot) => {
       snapshot.docChanges.filter(change => change.type === 'added')
-        .forEach(change => watchTx(change.doc, statusCallback));
+        .forEach(change => watchQueue.add(change.doc));
     });
-  startWatcher();
+  watchQueue.startWatch();
 }
 
 main();
