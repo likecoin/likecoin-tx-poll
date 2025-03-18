1fd3fa9f4938d47c34292ed17c40a96821d2db79
William Chong <me@williamchong.cloud>
2025-03-19 01:47:41 +0800

    🔨 Update docker build script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ WD=`cd $(dirname "$0") && pwd -P`
 
 cd "${WD}"
 
-docker build . -t like-txpoll:latest
+docker buildx build . -t like-txpoll:latest --platform linux/amd64
 docker tag like-txpoll:latest us.gcr.io/likecoin-foundation/like-txpoll:latest
 docker -- push us.gcr.io/likecoin-foundation/like-txpoll:latest
 
